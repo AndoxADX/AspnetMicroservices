a6b95ea4a164c5272d03ee86313504639ec1fca7
Amit Kumar <amitverma1150@gmail.com>
2021-04-04 19:50:08 +0530

    discount grpc complete

--- src/Services/Basket/Basket.API/Controllers/BasketController.cs
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -1,4 +1,5 @@
 ﻿using Basket.API.Entities;
+using Basket.API.GrpcServices;
 using Basket.API.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -12,12 +13,15 @@ namespace Basket.API.Controllers
     [Route("api/v1/[controller]")]
     public class BasketController : ControllerBase
     {
-        private readonly IBasketRepository repository;
+        private readonly IBasketRepository _repository;
+        private readonly DiscountGrpcService _discountGrpcService;
 
-        public BasketController(IBasketRepository repository)
+        public BasketController(IBasketRepository repository, DiscountGrpcService discountGrpcService)
         {
-            this.repository = repository ?? throw new ArgumentNullException(nameof(repository));
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+            _discountGrpcService = discountGrpcService ?? throw new ArgumentNullException(nameof(discountGrpcService));
         }
+
 
         //private readonly DiscountGrpcService _discountGrpcService;
         //private readonly IPublishEndpoint _publishEndpoint;
@@ -35,7 +39,7 @@ namespace Basket.API.Controllers
         [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
         public async Task<ActionResult<ShoppingCart>> GetBasket(string userName)
         {
-            var basket = await repository.GetBasket(userName);
+            var basket = await _repository.GetBasket(userName);
             return Ok(basket ?? new ShoppingCart(userName));
         }
 
@@ -46,20 +50,20 @@ namespace Basket.API.Controllers
             // TODO : Communicate with Discount.Grpc
             // and Calculate latest prices of product into shopping cart
             // consume Discount Grpc
-            //foreach (var item in basket.Items)
-            //{
-            //    var coupon = await _discountGrpcService.GetDiscount(item.ProductName);
-            //    item.Price -= coupon.Amount;
-            //}
+            foreach (var item in basket.Items)
+            {
+                var coupon = await _discountGrpcService.GetDiscount(item.ProductName);
+                item.Price -= coupon.Amount;
+            }
 
-            return Ok(await repository.UpdateBasket(basket));
+            return Ok(await _repository.UpdateBasket(basket));
         }
 
         [HttpDelete("{userName}", Name = "DeleteBasket")]
         [ProducesResponseType(typeof(void), (int)HttpStatusCode.OK)]
         public async Task<IActionResult> DeleteBasket(string userName)
         {
-            await repository.DeleteBasket(userName);
+            await _repository.DeleteBasket(userName);
             return Ok();
         }
 
@@ -75,7 +79,7 @@ namespace Basket.API.Controllers
             // remove the basket
 
             // get existing basket with total price
-            var basket = await repository.GetBasket(basketCheckout.UserName);
+            var basket = await _repository.GetBasket(basketCheckout.UserName);
             if (basket == null)
             {
                 return BadRequest();
@@ -87,7 +91,7 @@ namespace Basket.API.Controllers
             //await _publishEndpoint.Publish(eventMessage);
 
             // remove the basket
-            await repository.DeleteBasket(basket.UserName);
+            await _repository.DeleteBasket(basket.UserName);
 
             return Accepted();
         }
