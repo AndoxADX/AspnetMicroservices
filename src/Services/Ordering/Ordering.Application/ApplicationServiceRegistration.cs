8f12d868b3dc4c0f666fdecc03a9ac563d7151e6
Amit Kumar <amitverma1150@gmail.com>
2021-04-05 17:17:21 +0530

    Ordering.Application development

---
+++ src/Services/Ordering/Ordering.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Behaviours;
+using System.Reflection;
+
+namespace Ordering.Application
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            var assemblies = Assembly.GetExecutingAssembly();
+
+            services.AddAutoMapper(assemblies);
+            services.AddValidatorsFromAssembly(assemblies);
+            services.AddMediatR(assemblies);
+
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+
+            return services;
+        }
+    }
+}
